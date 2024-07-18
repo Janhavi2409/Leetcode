b56223beee6966fd380f2af27556d0df9063f24c
Janhavi2409 <janhavidahatonde@gmail.com>
2024-07-18 20:06:57 +0530

    Math

---
+++ src/Math/_69_SqrtX.java
@@ -0,0 +1,25 @@
+package Math;
+
+public class _69_SqrtX {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+
+        int start = 0;
+        int mid = -1;
+        int end = x;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if ((long) mid * mid > (long) x) {
+                end = mid - 1;
+            } else if (mid == x) {
+                return mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return Math.round(end);
+    }
+}
