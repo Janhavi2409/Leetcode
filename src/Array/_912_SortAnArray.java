1eb0b94e399cf7e39831f1257b16b75f90680f0b
Janhavi2409 <janhavidahatonde@gmail.com>
2024-07-25 14:31:30 +0530

    Arrays

---
+++ src/Array/_912_SortAnArray.java
@@ -0,0 +1,37 @@
+package Array;
+
+public class _912_SortAnArray {
+    static void quickSort(int[] nums, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+
+        int start = low;
+        int end = high;
+        int mid = start + (end - start) / 2;
+        int pivot = nums[mid];
+
+        while (start <= end) {
+            while (nums[start] < pivot) {
+                start++;
+            }
+            while (nums[end] > pivot) {
+                end--;
+            }
+            if (start <= end) {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        quickSort(nums, low, end);
+        quickSort(nums, start, high);
+    }
+
+    public int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+}
