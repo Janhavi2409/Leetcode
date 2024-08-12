6ac42116e4f902fcd1887c5c1e2be27562057ca4
Janhavi2409 <janhavidahatonde@gmail.com>
2024-08-12 21:57:05 +0530

    Strings

---
+++ src/Strings/_28_FindTheIndexOfFirstOccurrenceInAString.java
@@ -0,0 +1,7 @@
+package Strings;
+
+public class _28_FindTheIndexOfFirstOccurrenceInAString {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
