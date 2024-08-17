596fbe58a2b568cfe99af07f60a2d945ea3b4452
Janhavi2409 <janhavidahatonde@gmail.com>
2024-08-17 21:06:50 +0530

    Arrays

---
+++ src/Array/_136_SingleNumber.java
@@ -0,0 +1,18 @@
+package Array;
+
+import java.util.Arrays;
+
+public class _136_SingleNumber {
+    public int singleNumber(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return nums[nums.length - 1];
+    }
+}
