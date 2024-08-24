b791c49c2ab82dc1bb232bd9fc4a4644af5ec62f
Janhavi2409 <janhavidahatonde@gmail.com>
2024-08-24 21:07:35 +0530

    Arrays

--- src/Array/_283_MoveZeroes.java
+++ src/Array/_283_MoveZeroes.java
@@ -3,7 +3,7 @@ package Array;
 public class _283_MoveZeroes {
     public void moveZeroes(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; i < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] == 0) {
                     int temp = nums[i];
                     nums[i] = nums[j];
