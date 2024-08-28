053818b2cae208c03ec8ce307ca332ce27a43d81
Janhavi2409 <janhavidahatonde@gmail.com>
2024-08-28 12:57:46 +0530

    Arrays

---
+++ src/Array/_485_MaxConsecutiveOnes.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class _485_MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+        max = Math.max(max, count);
+        return max;
+    }
+}
