d56eb95669c51b2161e0523e48f066e9bb5d3e0d
Janhavi2409 <janhavidahatonde@gmail.com>
2026-01-01 10:19:29 +0530

    Math

---
+++ src/Math/_70_ClimbingStairs.java
@@ -0,0 +1,15 @@
+package Math;
+
+public class _70_ClimbingStairs {
+        public int climbStairs(int n) {
+            if (n <= 2)
+                return n;
+            int[] ways = new int[n];
+            ways[0] = 1;
+            ways[1] = 2;
+            for (int i = 2; i < n; i++) {
+                ways[i] = ways[i - 1] + ways[i - 2];
+            }
+            return ways[n - 1];
+        }
+}
