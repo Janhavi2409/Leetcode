0af46fb98663480558892974f5ce2f3b5eb19099
Janhavi2409 <janhavidahatonde@gmail.com>
2026-01-02 11:53:44 +0530

    Array

---
+++ src/Array/_1752_CheckIfArrayIsSortedAndRotated.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class _1752_CheckIfArrayIsSortedAndRotated {
+        public boolean check(int[] nums) {
+            int count = 0;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < nums[i - 1]) {
+                    count++;
+                }
+            }
+            if (nums[nums.length - 1] > nums[0]) {
+                count++;
+            }
+            if (count > 1) {
+                return false;
+            }
+            return true;
+        }
+}
